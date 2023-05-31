9c3a54dea1aa70d389d643d2eb93374a6be98018
ZloyMatvey <122729209+ZloyMatvey@users.noreply.github.com>
2023-05-31 21:07:35 +0300

    Update StatisticsServiceTest

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -8,7 +8,7 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
